d6fe147203a70b05fbaabf1f876f24c756c614ac
Anastasiia Morozova <nm@yl-verkot.com>
2022-01-25 15:18:56 +0200

    add xls test

--- src/test/java/qaguru_hw7/FilesTests.java
+++ src/test/java/qaguru_hw7/FilesTests.java
@@ -1,13 +1,21 @@
 package qaguru_hw7;
 
+import com.codeborne.xlstest.XLS;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class FilesTests {
+    @Disabled
     @Test
     @DisplayName("Загрузка jpg файла")
     void filenameShouldDisplayedAfterUploadActionFromClasspathTest() {
@@ -15,5 +23,24 @@ public class FilesTests {
         $("input[type='file']").uploadFromClasspath("not_pass.jpg");
         $("#uploadFile").click();
         $("#uploadedFilePath").shouldHave(text("C:\\fakepath\\not_pass.jpg"));
+    }
+
+    @Test
+    @DisplayName("Скачивание XLS-файла")
+    void xlsFileDownloadTest() throws IOException {
+        open("https://ckmt.ru/price-download.html");
+        File file = $$("a[href*='https://ckmt.ru/TehresursPrice.xls']")
+                .find(text("Скачать"))
+                .download();
+
+        XLS parsedXls = new XLS(file);
+        boolean checkPassed = parsedXls.excel
+                .getSheetAt(0)
+                .getRow(8)
+                .getCell(3)
+                .getStringCellValue()
+                .contains("от упаковки");
+
+        assertTrue(checkPassed);
     }
 }
